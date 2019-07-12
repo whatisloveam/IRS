181bcce7db59eeb19dc2cc01e70082cd69d9fab8
WhatIsLove <vladi2000@hotmail.com>
2019-07-12 21:37:54 +0500

    errors fix

--- Lines.ino
+++ Lines.ino
@@ -44,34 +44,3 @@ void LineForCross(int power) // really? I have nothing to explain in this case
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void UpdSen()
-{
-  s[0] = map(analogRead(A0), maxi[0], mini[0], 0, 100); // 0 1 2 3
-  s[1] = map(analogRead(A1), maxi[1], mini[1], 0, 100);
-  s[2] = map(analogRead(A2), maxi[2], mini[2], 0, 100);
-  s[3] = map(analogRead(A3), maxi[3], mini[3], 0, 100);  
-  s[4] = map(analogRead(A6), maxi[4], mini[4], 0, 100);  
-}
